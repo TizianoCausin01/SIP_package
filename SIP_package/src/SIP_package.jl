a708ed50634a19103b43968c4fa9daa38cb26b5b
TizianoCausin01 <causintiziano@gmail.com>
2024-12-07 19:13:26 +0100

    created package SIP_package that include all functions up to the sampling

---
+++ SIP_package/src/SIP_package.jl
@@ -0,0 +1,304 @@
+module SIP_package
+
+# =========================
+# EXPORTED FUNCTIONS    
+# =========================
+
+export split_vid,
+       get_new_dimensions,
+       get_cutoff,
+       glider_coarse_g,
+       glider,
+       get_nth_window
+
+# =========================
+# IMPORTED PACKAGES
+# =========================
+
+using Images,
+      VideoIO,
+      FFMPEG,
+      Statistics,
+      HDF5,
+      ImageView
+
+
+# =========================
+# PREPROCESSING
+# =========================
+
+"""
+split_vid
+Splits the video in pieces of the specified segment_duration 
+and then saves them in the specified output_name
+Inputs :
+- file_name -> complete path to the file
+- output_name -> complete path for the output 
+                (must have %03d to save them in progressive order
+                starting from 000 like in python)
+- segment_duration -> how long should the computed segment be in seconds
+                        (the last could inevitably be shorter)
+
+breakdown of cmd
+cmd =`        # backtick to start the command
+ffmpeg        
+-i file_name # input
+-an           # excludes the audio channel : not needed
+-c:v copy     # codec only video, copy directly from the compressed one (without encoding and decoding)
+-f segment    # format : segment of video
+-segment_time segment_duration # duration of each segment
+-reset_timestamps 1 # every segment will have independent timestamps starting from 00:00:00
+output_name  
+`
+"""
+function split_vid(file_name, output_name, segment_duration)
+
+    cmd = `
+    /opt/homebrew/bin/ffmpeg
+    -i $file_name
+    -an
+    -c:v copy
+    -f segment
+    -segment_time $segment_duration
+    -reset_timestamps 1
+    $output_name
+`
+    run(cmd) # runs the command
+end # EOF
+
+
+"""
+video_conversion
+reads and converts the video starting from the file name
+uses color2BW and get_dimensions as functions
+"""
+function video_conversion(file_name)   
+    # loads and opens the video
+    # video_load = VideoIO.load(file_name) # to add if you will convert directly the videos from the internet
+    reader = VideoIO.openvideo(file_name)
+    # stores quantities for preallocation
+    frame_1, height, width, frame_num = get_dimensions(reader) # reads the first frame to get the dimensions of the movie and returns what you see in variable assignment
+    BW_vid = BitArray(undef, height, width, frame_num) # preallocates a boolean matrix of zeros, s.t. we then substitute ones where gray_frame > gray_median is true 
+    fill!(BW_vid,false)
+    #BW_vid = BitArray(BW_vid_bool);
+    count = 1 # index for later storing frames, starts from one because we already read the first frame
+    BW_vid[:,:,count] = color2BW(frame_1) # special treatment for frame_1 because we have already read it
+
+    while !eof(reader) # it loops until the last frame has been read
+        count += 1 # updates the count
+        frame = VideoIO.read(reader)  # reads the movie
+        BW_vid[:,:,count] = color2BW(frame) # assigns the binarized frame to the array
+    end # while 
+    close(reader) # closes the reader
+    # bit_vid = BitArray(BW_vid) # converts the bool vid into a BitArray for optimization
+    return BW_vid
+end # EOF
+    
+
+"""
+get_dimensions
+reads the first frame of the frame of the movie and gets the dimensions of the movie
+"""
+function get_dimensions(reader)
+    frame_num = VideoIO.counttotalframes(reader) # total number of frames
+    frame_1 = VideoIO.read(reader) # stores the first frame to get the height and width
+    height, width = size(frame_1) 
+    return frame_1, height, width, frame_num 
+end # EOF
+
+
+"""
+color2BW
+converts an RGB array into a BitArray
+"""
+function color2BW(frame)
+    gray_frame = Gray.(frame) # converts the frame from RGB to grayscale
+    gray_median = median(gray_frame) 
+    BW_frame= gray_frame .> gray_median # it is true(=1=white) only where the grayscale value is greater than the median. Then it broadcasts the ones in the correct positions of the array. The zeros are already there.
+    return BW_frame
+end # EOF
+
+# =========================
+# COARSE-GRAINING
+# =========================
+
+"""
+get_new_dimensions
+Derives the new dimensions of the coarse grained video.
+Inputs :
+- video_dim -> the dimensions of the previous iteration 
+- coarse_g_dim -> the dimensions of the coarse graining to be done
+Outputs :
+- new_dim -> the new dimensions of the coarse grained video
+"""    
+function get_new_dimensions(video_dim, coarse_g_dim)
+    new_dim = (                                     # floor s.t it won't overindex
+        floor(Int, video_dim[1]/coarse_g_dim[1]),
+        floor(Int, video_dim[2]/coarse_g_dim[2]),
+        floor(Int, video_dim[3]/coarse_g_dim[3]),
+    )
+    return new_dim
+end # EOF
+
+
+"""
+get_cutoff
+Gets the dimensions of the glider for coarse graining
+and returns a cutoff value for the decision rule
+Inputs : 
+- glider_coarse_g_dim -> tuple with the dimensions of the glider for coarse graining 
+"""
+function get_cutoff(glider_coarse_g_dim)   
+    volume = glider_coarse_g_dim[1] * glider_coarse_g_dim[2] * glider_coarse_g_dim[3] #computes the volume of the solid
+    cutoff = volume / 2 # sets the cutoff for the majority rule 
+    return cutoff
+end # EOF
+    
+
+"""
+glider_coarse_g
+It's the glider for coarse graining. Loops over all the steps 
+and returns a new video which is the old video coarse grained. 
+Inputs :
+- bin_vid -> the binarized video from the previous iteration
+- tot_steps -> a tuple created with get_steps which has 3 
+                arrays of numbers, one for each dimension. It 
+                indicates the onset of each new step
+- glider_coarse_g_dim -> tuple with the dimensions of the coarse graining
+- cutoff -> given the dimensions of the glider, the cutoff for the majority rule
+
+Outputs : 
+- new_vid -> the new coarse-grained video
+    """
+function glider_coarse_g(bin_vid, new_vid, tot_steps, glider_coarse_g_dim, cutoff)
+    counter_new_mat = [0, 0, 0] # rows, cols, depth initializes a new counter for indexing in the new matrix
+    for i_time = tot_steps[3]
+        idx_time = i_time : i_time + glider_coarse_g_dim[3] - 1 # you have to subtract one, otherwise you will end up getting a bigger glider
+        counter_new_mat[3] += 1 # updates the counter accordingly
+        counter_new_mat[1] = 0 # sets the counter of the inner loop to zero s.t. it doesn't overindex
+        for i_rows = tot_steps[1]
+            idx_rows = i_rows : i_rows + glider_coarse_g_dim[1] - 1
+            counter_new_mat[1] += 1
+            counter_new_mat[2] = 0 # sets the counter of the inner loop to zero s.t. it doesn't overindex
+            for i_cols = tot_steps[2]
+                idx_cols = i_cols : i_cols + glider_coarse_g_dim[2] - 1
+                counter_new_mat[2] += 1
+                white_count = sum(bin_vid[idx_rows, idx_cols, idx_time]) # index in video, gets the current window and immediately sums over it. 
+                new_vid[counter_new_mat[1], counter_new_mat[2], counter_new_mat[3]] = majority_rule(white_count, cutoff) # assigns the pixel of the coarse grained video in the correct position
+            end # cols
+        end # rows
+    end # time
+    return new_vid
+
+end # EOF
+
+
+"""
+compute_steps_glider
+Creates a tuple with the steps the glider will have to do in each dimension. 
+Each number in the list is the initial element in the new window. 
+It subtracts the glider_dim such that we won't overindex
+""" 
+function compute_steps_glider(glider_dim, video_dim)
+    tot_steps = (
+                    1 : glider_dim[1] : video_dim[1] - glider_dim[1],
+                    1 : glider_dim[2] : video_dim[2] - glider_dim[2],
+                    1 : glider_dim[3] : video_dim[3] - glider_dim[3]
+                ) 
+    return tot_steps
+end # EOF
+
+
+""" 
+majority_rule
+receives how many white pixels there are in the window 
+and then turns the whole picture into either black or white
+according to the majority rule 
+Inputs :
+- white_count -> count of white pixels in the window
+- cutoff -> half of the total pixels in the window taken into account
+Outputs :
+- pix -> how the pixel in the coarse grained video will look like
+"""
+function majority_rule(white_count, cutoff)
+    if white_count > cutoff
+        pix = true
+    else
+        pix = false
+    end # end if
+    return pix
+end # EOF
+
+# =========================
+# SAMPLING
+# =========================
+
+
+"""
+glider
+Creates a glider that slides over the given binarized video 
+and counts the configuration occurrences. Step=1
+Inputs : 
+- bin_vid -> the binarized (BitArray) video
+- glider_dim -> a tuple with the three dimensions of the glider
+
+Outputs :
+- counts -> it's a dict with BitVector as keys and Int as values. 
+            It stores the counts of windows configurations
+"""
+function glider(bin_vid, glider_dim)
+    counts = Dict{BitVector, Int}()
+    vid_dim = size(bin_vid)
+    for i_time = 1 : vid_dim[3] - glider_dim[3] # step of sampling glider = 1
+        idx_time = i_time : i_time + glider_dim[3] - 1 # you have to subtract one, otherwise you will end up getting a bigger glider
+        for i_rows = 1 : vid_dim[1] - glider_dim[1]
+            idx_rows = i_rows : i_rows + glider_dim[1] - 1
+            for i_cols = 1 : vid_dim[2] - glider_dim[2]
+                idx_cols = i_cols : i_cols + glider_dim[2] - 1
+                window = vec(bin_vid[idx_rows, idx_cols, idx_time]) # index in video, gets the current window and immediately vectorizes it. 
+                counts = update_count(counts, window)
+            end # cols
+        end # rows
+    end # time
+    return counts
+end # EOF
+
+
+"""
+update_count
+updates the dictionary of counts such that if the window was already
+present, it adds 1 to the value (to count it), otherwise it creates it
+and initializes its value with a 1
+"""    
+function update_count(counts_dict, window)
+    if haskey(counts_dict, window)
+        counts_dict[window]+=1
+    else
+        counts_dict[window] = 1
+    end # if has key 
+    return counts_dict
+end #EOF
+
+
+"""
+get_nth_window
+From Vector{Pair{BitVector, Int}} which is the counts_dict 
+after it has been sorted, this function gets the nth top window, 
+unflattens it and turns it into gray values to be visualized 
+inputs : 
+- idx -> the rank of the window, in descending order
+- sorted_counts -> the counts_dict after it has been sorted, 
+    type: Vector{Pair{BitVector, Int}}
+- glider_dim -> the dimensions of the glider, to unflatten the vectorized window
+"""
+function get_nth_window(idx, sorted_counts, glider_dim)
+    window = sorted_counts[idx].first # gets the window from the dict
+    r_window = reshape(window, glider_dim) # reshapes it according to the glider dimensions
+    gray_window = Gray.(r_window) # turns it into gray to visualize it
+    count = sorted_counts[idx].second
+    return gray_window, count
+end # EOF
+
+
+
+end # module SIP_package
