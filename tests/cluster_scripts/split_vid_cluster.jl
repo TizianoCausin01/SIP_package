6cfa8d8c4eade54a67b33d9a9128d0f84194624f
Tiziano Causin <tcausin0@login07.leonardo.local>
2025-03-29 10:09:28 +0100

    added parsing arguments for cluster

--- tests/cluster_scripts/split_vid_cluster.jl
+++ tests/cluster_scripts/split_vid_cluster.jl
@@ -5,5 +5,5 @@ Pkg.activate(".")
 using SIP_package
 path2data = "/leonardo_scratch/fast/Sis25_piasini/tcausin/SIP_data/"
 file_name = ARGS[1]
-chunk_duration = ARGS[2]
+chunk_duration = parse(Int, ARGS[2])
 split_vid(path2data, file_name, chunk_duration)
