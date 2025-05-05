8e7fff02c334f2800e13a04ca09d10d32a542242
Tiziano Causin <tcausin0@login07.leonardo.local>
2025-05-05 08:36:09 +0200

    Added successfully possibility to send large data in the convergence function

--- tests/cluster_scripts/cluster_merge_and_converge_real.jl
+++ tests/cluster_scripts/cluster_merge_and_converge_real.jl
@@ -217,11 +217,13 @@ elseif in(rank, mergers) # I am merger
 			end # if src_worker == -1
 		end # if ismessage
 	end # while stop == 0
+if isdefined(Main, :tot_dicts) && tot_dicts !== nothing # checkpoints
 	for iter_idx in 1:num_of_iterations
 		open("$(results_folder)/counts_$(name_vid)_iter$(iter_idx)_rank$(rank).json", "w") do file # the folder has to be already present 
 			JSON.print(file, tot_dicts[iter_idx])
 		end # open counts
 	end # for iter_idx in 1:num_of_iterations
+end # if isdefined(Main, :tot_dicts)
 	mergers_convergence(rank, mergers, tot_dicts, num_of_iterations, results_folder, name_vid, comm)
 else # I am worker
 	@info "proc $(rank): I am worker"
