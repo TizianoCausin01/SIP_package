f3d7425ddc62986d9ea1e09fc9cb2b0ffec1c8fd
Tiziano Causin <tcausin0@login01.leonardo.local>
2025-05-24 19:22:50 +0200

    Created script to submit different videos together

---
+++ tests/cluster_scripts/sampling_general.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=32 # number of processes
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=480G
+#SBATCH --account=Sis25_piasini       # account name
+#SBATCH --partition=boost_usr_prod # partition name
+#SBATCH --output=/leonardo_scratch/fast/Sis25_piasini/tcausin/log_exchange/%x.%j.out   # file name will be *job_name*.*job_id*
+
+cd /leonardo/home/userexternal/epiasini/sissa/tiziano/SIP_package
+fn=${1}
+cg1=${2}
+cg2=${3}
+cg3=${4}
+win1=${5}
+win2=${6}
+win3=${7}
+mergers_num=${8}
+module load openmpi # hdf5
+export JULIA_NUM_THREADS=1
+
+heap_size_hint=10G
+
+time mpiexec --bind-to core --map-by core -np $SLURM_NTASKS stdbuf -o0 -e0 julia --project --heap-size-hint=$heap_size_hint tests/cluster_scripts/cluster_merge_and_converge_real.jl $fn $cg1 $cg2 $cg3 $win1 $win2 $win3 $mergers_num
+
