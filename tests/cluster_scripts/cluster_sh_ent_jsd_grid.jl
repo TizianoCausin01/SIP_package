39cf5c7a5fddcc6dae5f03d986c875f576c6b80f
Tiziano Causin <tcausin0@login01.leonardo.local>
2025-05-30 17:09:48 +0200

    Modified the cluster version of jsd and sh entropy computation

--- tests/cluster_scripts/cluster_sh_ent_jsd_grid.jl
+++ tests/cluster_scripts/cluster_sh_ent_jsd_grid.jl
@@ -5,13 +5,13 @@ Pkg.activate(".")
 using SIP_package
 using DelimitedFiles
 ##
-results_path = "/leonardo_scratch/fast/Sis25_piasini/tcausin/SIP_results"
+results_path = "/leonardo_work/Sis25_piasini/tcausin/SIP_results"
 file_name = ARGS[1]
 cg_dims = Tuple(parse(Int, ARGS[i]) for i in 2:4)
 win_dims = Tuple(parse(Int, ARGS[i]) for i in 5:7) # rows, cols, depth
 iterations_num = 5
 counts_path = "$(results_path)/$(file_name)_counts_cg_$(cg_dims[1])x$(cg_dims[2])x$(cg_dims[3])_win_$(win_dims[1])x$(win_dims[2])x$(win_dims[3])"
-tot_prob_dicts = [counts2prob(json2dict("$(counts_path)/counts_$(file_name)_iter$(iter).json"), 8) for iter in 1:iterations_num]
+tot_prob_dicts = [counts2prob(json2intdict("$(counts_path)/counts_$(file_name)_iter$(iter).json"), 8) for iter in 1:iterations_num]
 @info "dicts converted"
 ##
 sh_ent_vec = [tot_sh_entropy(tot_prob_dicts[iter]) for iter in 1:iterations_num]
